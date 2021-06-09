6e19a5bb2e606209940baf8f5b8d30fced841101
Jaeyeob KIM <59470033+YeobKim@users.noreply.github.com>
2021-06-09 21:31:16 +0900

    Update bowling.py

--- 이코테/part11/bowling.py
+++ 이코테/part11/bowling.py
@@ -2,16 +2,16 @@
 볼링공 고르기
 permutations를 이용해서 조합을 구한다음 같은 무게인 것만 제외
 """
-from itertools import permutations
+from itertools import combinations
 
 n, m = map(int, input().split())
 ball_w = list(map(int, input().split()))
 
-per = list(permutations(ball_w, 2))
-print(per)
+com = list(combinations(ball_w, 2))
+print(com)
 
-for x in per:
+for x in com:
     if x[0] == x[1]:
-        per.remove(x)
+        com.remove(x)
 
-print(len(per)//2)
+print(len(com))
