dcf864a820a4a9a7f0c4cfe8235ae38f72b3f28e
Jaeyeob KIM <athurk94111@gmail.com>
2021-10-26 00:23:18 +0900

    Update test_answer.py

--- Programmers/모의고사/test_answer.py
+++ Programmers/모의고사/test_answer.py
@@ -7,14 +7,17 @@ def solution(answers):
     answer_cnt = []
     
     for i in range(len(answers)):
-        if answers[i] == p1[i]:
+        # i%len(p1)처럼 해서 정의한 p1의 길이보다 벗어난 answers가 들어오더라도 다시 시작하면서 반복할 수 있도록 구성
+        if answers[i] == p1[i%len(p1)]:
             cnt1 += 1
-        if answers[i] == p2[i]:
+        if answers[i] == p2[i%len(p2)]:
             cnt2 += 1
-        if answers[i] == p3[i]:
+        if answers[i] == p3[i%len(p3)]:
             cnt3 += 1
     answer_cnt = [cnt1, cnt2, cnt3]
     
+    max_score = max(answer_cnt)
+
     for person, score in enumerate(answer_cnt):
         if score == max(answer_cnt):
             answer.append(person+1)
